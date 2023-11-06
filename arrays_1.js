9884177c559e3953fd10fcbd81955055285ba70d
JoshuaPagel <128010152+JoshuaPagel@users.noreply.github.com>
2023-11-05 17:52:37 -0800

    first commit

---
+++ arrays_1.js
@@ -0,0 +1,36 @@
+// Given an array and an additional value, insert this value at the beginning of the array.
+
+// let Arr = [5, 7, 2, 8, 5];
+
+// console.log("Array Elements: " + Arr);
+
+// function pushFront() {
+//     Arr.unshift("8");
+//     console.log("New Array Elements: " + Arr);
+// }
+
+// pushFront();
+
+// Given an array, remove and return the value at the beginning of the array. Prove the value is removed from the array by printing it
+
+// function popFront() {
+//     Arr.shift();
+//     console.log("New Array Elements: " + Arr)
+// }
+
+// popFront();
+
+// Given an array, index, and additional value, insert the value into array at given index. You can think of pushFront(arr,val) as equivalent to insertAt(arr,0,val).
+
+
+// function insertValue() {
+//     let index = 2;
+//     let element = -99;
+
+//     Arr.splice(index, 0, element);
+//     console.log(Arr);
+// }
+
+// insertValue();
+
+
